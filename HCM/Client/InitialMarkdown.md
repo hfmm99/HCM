597eed2389e5e7c72016975e8f056a4c11c0e08d
YisusDeveloper <52508211+YisusDeveloper@users.noreply.github.com>
2019-11-27 16:20:57 -0600

    Added Markdown render page

---
+++ HCM/Client/InitialMarkdown.md
@@ -0,0 +1,18 @@
+﻿#Heading
+###Sub-heading
+---
+This is some sample markdown, with common things like _italics_, **bold**, ~~strikethrough~~, etc...
+
+1. List Item 1
+    -unordered sublist item
+    -unordered sublist item
+2.Item 2 with emphasis
+3.Item 3 with inline code
+
+---
+Here's a block of code:
+```
+var x = 1;
+var y = 2;
+var sum = x + y;
+```
--- HCM/Client/Startup.cs
+++ HCM/Client/Startup.cs
@@ -1,3 +1,4 @@
+using Ganss.XSS;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +9,16 @@ namespace HCM.Client
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddBlazorModal();
+            services.AddScoped<IHtmlSanitizer, HtmlSanitizer>(x =>
+            {
+                /*Configure sanitizer rules as needed here.
+                For now, just use default rules + allow class attributes*/
+                var sanitizer = new Ganss.XSS.HtmlSanitizer();
+                sanitizer.AllowedAttributes.Add("class");
+                return sanitizer;
+            });
         }
+
 
         public void Configure(IComponentsApplicationBuilder app)
         {
