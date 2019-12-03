100aeee28a769d3080800860d0804b3d5be9e7cb
YisusDeveloper <52508211+YisusDeveloper@users.noreply.github.com>
2019-12-03 09:13:34 -0600

    Markdown view

---
+++ HCM/Client/Components/Markdown/MarkdownOutput.razor.cs
@@ -0,0 +1,46 @@
+﻿using Ganss.XSS;
+using Markdig;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Components;
+using HCM.Client.Services;
+using System;
+
+namespace HCM.Client.Components.MarkDown
+{
+    public class MarkDownModel : ComponentBase
+    {
+        private string _content;
+
+        [Inject] public IHtmlSanitizer HtmlSanitizer { get; set; }
+
+        [Parameter]
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                HtmlContent = ConvertStringToMarkupString(_content);
+            }
+        }
+
+        public MarkupString HtmlContent { get; private set; }
+
+        private MarkupString ConvertStringToMarkupString(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(_content))
+            {
+                // Convert markdown string to HTML
+                var html = Markdig.Markdown.ToHtml(value, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+
+                // Sanitize HTML before rendering
+                var sanitizedHtml = HtmlSanitizer.Sanitize(html);
+
+                // Return sanitized HTML as a MarkupString that Blazor can render
+                return new MarkupString(sanitizedHtml);
+            }
+
+            return new MarkupString();
+        }
+    }
+}
