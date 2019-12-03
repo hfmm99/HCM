d356c9c49537262f54c2fe7d1984144154473f24
YisusDeveloper <52508211+YisusDeveloper@users.noreply.github.com>
2019-12-03 09:16:37 -0600

    Markdownview

---
+++ HCM/Client/Pages/Item.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Components;
+
+namespace HCM.Client
+{
+    public class ItemModel : ComponentBase
+    {
+        public string Title => "Render Markdown";
+
+        public string MarkdownContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            MarkdownContent = GetInitialMarkdown();
+        }
+
+        private string GetInitialMarkdown()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().Single(x => x.EndsWith("InitialMarkdown.md"));
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
