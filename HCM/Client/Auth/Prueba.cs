04018342e3f1f6c7801fbf031f013daaa3eaacaf
bzuniga <bzuniga@ZUNIGAB>
2020-02-07 10:48:19 -0600

    Cambios 7/2/2020

--- HCM/Client/Auth/Prueba.cs
+++ HCM/Client/Auth/Prueba.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
