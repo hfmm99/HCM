3f6d66ea0b93a5733c7008e74d02ee245016afd7
bzuniga <bzuniga@ZUNIGAB>
2020-02-21 16:30:10 -0600

    Add filter to show only the events created by loged user. Parameters for Score Rating component

--- HCM/Client/Components/ScoreRating/ScoreRatingOption.cs
+++ HCM/Client/Components/ScoreRating/ScoreRatingOption.cs
@@ -8,12 +8,23 @@ namespace HCM.Client.Components.ScoreRating
 {
     public class ScoreRatingOption : ComponentBase
     {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public string Color { get; set; }
+
+        [Parameter]
+        public int Value { get; set; }
+
+        [Parameter]
+        public bool Checked { get; set; }
+    
         [CascadingParameter()]
         public ScoreRating ScoreRating { get; set; }
 
         protected override Task OnInitializedAsync()
         {
-            Console.WriteLine(ScoreRating != null);
             ScoreRating.Options.Add(this);
             return base.OnInitializedAsync();
         }
